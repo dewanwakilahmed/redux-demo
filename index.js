35ea106508360f9e73534a1e0664923e8c62c124
Dewan Wakil Ahmed <dewanwakilahmed@gmail.com>
2022-01-24 18:18:38 +0600

    Combine Reducers

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const redux = require("redux");
 const createStore = redux.createStore;
+const combineReducers = redux.combineReducers;
 
 const BUY_CAKE = "BUY_CAKE";
 const BUY_ICECREAM = "BUY_ICECREAM";
@@ -19,11 +20,6 @@ function buyIceCream() {
 
 // (previousState, action) => newState;
 
-// const initialState = {
-//   numOfCakes: 10,
-//   numOfIceCreams: 20,
-// };
-
 const initialCakeState = {
   numOfCakes: 10,
 };
@@ -31,25 +27,6 @@ const initialCakeState = {
 const initialIceCreamState = {
   numOfIceCreams: 20,
 };
-
-// const reducer = (state = initialState, action) => {
-//   switch (action.type) {
-//     case BUY_CAKE:
-//       return {
-//         ...state,
-//         numOfCakes: state.numOfCakes - 1,
-//       };
-
-//     case BUY_ICECREAM:
-//       return {
-//         ...state,
-//         numOfIceCreams: state.numOfIceCreams - 1,
-//       };
-
-//     default:
-//       return state;
-//   }
-// };
 
 const cakeReducer = (state = initialCakeState, action) => {
   switch (action.type) {
@@ -77,7 +54,12 @@ const iceCreamReducer = (state = initialIceCreamState, action) => {
   }
 };
 
-const store = createStore(reducer);
+const rootReducer = combineReducers({
+  cake: cakeReducer,
+  iceCream: iceCreamReducer,
+});
+
+const store = createStore(rootReducer);
 
 console.log("Initial State", store.getState());
 
