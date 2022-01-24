ede3d1405098f4dfabd224b0eb6cdfa621b8a269
Dewan Wakil Ahmed <dewanwakilahmed@gmail.com>
2022-01-24 17:10:01 +0600

    Reducers

--- index.js
+++ index.js
@@ -6,3 +6,22 @@ function buyCake() {
     info: "First redux action",
   };
 }
+
+// (previousState, action) => newState;
+
+const initialState = {
+  numOfCakes: 10,
+};
+
+const reducer = (state = initialState, action) => {
+  switch (action.Type) {
+    case BUY_CAKE:
+      return {
+        ...state,
+        numOfCakes: state.numOfStates - 1,
+      };
+
+    default:
+      return state;
+  }
+};
